10b12d4d9b2239701c5a172e787ec6a503658122
yhidetoshi <hyajima.1014@gmail.com>
2018-07-18 19:12:04 +0900

    インスタンス名を取得可能に

--- test-apis/ec2/ec2.js
+++ test-apis/ec2/ec2.js
@@ -7,17 +7,24 @@ var ec2 = new AWS.EC2();
 
 ec2.describeInstances({}, function(err, data) {
 
-  var instance
-  var res = 0
-  var index = 0
+  var instance, reservation, name
+  var res, index, tag = 0
 
   for(index in data.Reservations){
     var reservation = data.Reservations[index];
 
+    // loop for number of instances
     for(res in reservation.Instances){
-
       instance = reservation.Instances[res];
       console.log(instance.InstanceId)
+
+      // get instance name
+      for (tag in instance.Tags){
+        if(instance.Tags[tag].Key === 'Name'){
+            name = instance.Tags[tag].Value;
+        }
+        console.log(name)
+      }
     }
   }
 
