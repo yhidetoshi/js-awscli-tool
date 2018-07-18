24d83e17613f1f9a56046f1dabee24bba8b69ede
yhidetoshi <hyajima.1014@gmail.com>
2018-07-18 19:05:35 +0900

    ec2のInstance-Idを取得可能に

--- test-apis/ec2/ec2.js
+++ test-apis/ec2/ec2.js
@@ -1,11 +1,24 @@
 var AWS = require('../../node_modules/aws-sdk');
 
-var ec2 = new AWS.EC2();
-var params = {};
+AWS.config.update({region: 'ap-northeast-1'});
 
-ec2.describeInstances(params, function(err, data) {
-  for (var index in data.Reservations){
-    var instances = data.Reservations[index];
-    console.log(instances);
+var ec2 = new AWS.EC2();
+
+
+ec2.describeInstances({}, function(err, data) {
+
+  var instance
+  var res = 0
+  var index = 0
+
+  for(index in data.Reservations){
+    var reservation = data.Reservations[index];
+
+    for(res in reservation.Instances){
+
+      instance = reservation.Instances[res];
+      console.log(instance.InstanceId)
+    }
   }
+
 });
